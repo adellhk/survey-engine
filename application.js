d40ec20e7b68faa0c43548fc060868b93d6be57e
Adell Hanson-Kahn <adell@adell.io>
2015-07-20 15:33:45 -0700

    fixed a few typos, mostly added radial, drop-down, and multi-select #isChoicesValid

--- application.js
+++ application.js
@@ -1,53 +1,65 @@
 $(document).ready(function() {
-	console.log("Hello, Aja.");
-	console.log(questionResponseTest());
+	console.log('Hello, Aja.');
+	questionResponseTest();
 });
 
 function Question(options) {
-	if(!options['questionText']) throw('Question text is required');
-	if(!options['questionType']) throw('Question type is required');
+	if (!options['questionText']) throw('Question text is required.');
+	if (!options['questionType']) throw('Question type is required.');
 
 	this.questionText = options['questionText'];
 	this.questionType = options['questionType'];
-	this.choices = options['choices']; // can be null, eg. open-ended question
-	this.responses = [];
+	this.questionChoices = options['questionChoices']; // can be null, eg. open-ended question
+	this.questionResponses = [];
 };
 
 Question.prototype.insertResponse = function(response) {
 	if (response instanceof(Response)) {
-		this.responses.push(response);
-		this.responses[this.responses.length-1].question = this;
+		this.questionResponses.push(response);
+		this.questionResponses[this.questionResponses.length-1].question = this;
 	} else {
-		throw "Response not a response.";
+		throw 'Supplied response is not an instance of response.';
 	};
 };
 
-Question.prototype.validate = function() {
-	if (
-		this.questionText.length > 0 &&
-		0==0
-		) {
+Question.prototype.isValid = function() {
+	if (!this.questionText.length > 0) return false;
+	if (['radial','drop-down','multi-select'].indexOf(this.questionType) != -1) {
+		return this.isChoicesValid();
+	};
+};
 
-	}
-}
+Question.prototype.isChoicesValid = function() {
+	if (!this.questionChoices || !this.questionChoices.length > 0) throw('A ' + this.questionType + ' question must include at least one choice.');
+	return true;
+};
 
 function Response(response) {
-	if (typeof response === "string") {
+	if (typeof response === 'string') {
 		this.text = response;
 	} else {
-		throw "Response must initialize with an response string.";
+		throw 'Response must initialize with a response string.';
 	};
 	this.question = null;
 };
 
 function questionResponseTest() {
 	var sky = new Question({
-		questionText: "What color is the sky?",
-		questionType: "radial"
-});
-	var blue = new Response("Blue");
-	var red = new Response("Red");
+		questionText: 'What color is the sky?',
+		questionType: 'open-ended'
+	});
+	var blue = new Response('Blue');
+	var red = new Response('Red');
 	sky.insertResponse(red);
 	sky.insertResponse(blue);
-	return sky;
+	sky.isValid();
+	console.log(sky);
+
+	var badRadial = new Question({
+		questionText: 'How many fl. oz in a liter?',
+		questionType: 'radial',
+		questionChoices: []
+	});
+
+	// badRadial.isValid(); // expect(function() {badRadial.isValid();} ).toThrow("A radial question must include at least one choice.");
 };
